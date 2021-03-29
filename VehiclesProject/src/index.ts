ad18e13b8c89b8b1c76c1da8d2ce4415384aab8c
Krauser123 <66463181+Krauser123@users.noreply.github.com>
2021-03-29 12:10:51 +0200

    Create index.ts

---
+++ VehiclesProject/src/index.ts
@@ -0,0 +1,6 @@
+import Vehicle from './Vehicle';
+import { createCar, Car } from './Car';
+import { createBike, Bike } from './Bike';
+
+//export default Vehicle;
+export { Vehicle, createCar, Car, createBike, Bike };
